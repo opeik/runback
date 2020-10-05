0d80e6976d1b7138e8bacaccf2c14a017b96d606
Sandro Stikić <sandro@stikic.me>
2020-10-05 19:21:41 +0800

    Added random load messages

--- src/dashboard/loading/main.vue
+++ src/dashboard/loading/main.vue
@@ -3,9 +3,6 @@
     <v-main>
       <v-container fluid fill-height>
         <v-row align="center" justify="center">
-          <v-col cols="12" class="text-center mb-8">
-            <span class="text-h1">Runback</span>
-          </v-col>
           <v-col cols="12" class="text-center">
             <v-progress-circular
               :size="100"
@@ -14,8 +11,11 @@
               color="primary"
             ></v-progress-circular>
           </v-col>
-          <v-col cols="12" class="text-center mt-8">
-            <span class="text-h5">Starting server... </span>
+          <v-col cols="12" class="text-center mt-6 mb-n4">
+            <span class="text-h4"> {{ load_quote }} </span>
+          </v-col>
+          <v-col cols="12" class="text-center">
+            <span class="text-h6">Starting server... </span>
           </v-col>
         </v-row>
       </v-container>
@@ -27,5 +27,22 @@
 import { Vue, Component } from "vue-property-decorator"
 
 @Component
-export default class extends Vue {}
+export default class extends Vue {
+  load_quotes = [
+    "Parrying your super",
+    "Baiting a burst",
+    "Ki charging",
+    "Explaining the DHC glitch",
+  ]
+
+  get_random_int(min: number, max: number): number {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min + 1)) + min
+  }
+
+  get load_quote(): string {
+    return this.load_quotes[this.get_random_int(0, this.load_quotes.length - 1)]
+  }
+}
 </script>
