aa881ade4cd9fe89143c591f11e02055e0e957f6
Sandro Stikić <sandro@stikic.me>
2020-10-05 19:09:38 +0800

    Add load screen text

--- src/dashboard/loading/main.vue
+++ src/dashboard/loading/main.vue
@@ -3,12 +3,19 @@
     <v-main>
       <v-container fluid fill-height>
         <v-row align="center" justify="center">
-          <v-col class="text-center">
+          <v-col cols="12" class="text-center mb-8">
+            <span class="text-h1">Runback</span>
+          </v-col>
+          <v-col cols="12" class="text-center">
             <v-progress-circular
               :size="100"
+              :width="7"
               indeterminate
               color="primary"
             ></v-progress-circular>
+          </v-col>
+          <v-col cols="12" class="text-center mt-8">
+            <span class="text-h5">Starting server... </span>
           </v-col>
         </v-row>
       </v-container>
