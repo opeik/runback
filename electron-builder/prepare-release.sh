71108e73f9e02f430872b8d9861ffab3ed26a791
Sandro Stikić <sandro@stikic.me>
2020-10-14 17:08:56 +0800

    Tweak build order

--- electron-builder/prepare-release.sh
+++ electron-builder/prepare-release.sh
@@ -5,7 +5,6 @@ set -euxo pipefail
 mkdir -p build/nodecg/bundles/runback
 cd lib/nodecg && npm install && cd ../..
 cp -rf lib/nodecg/{index.js,build,lib,schemas,src,types,package-lock.json} build/nodecg/
-npm run-script build
 cp -rf {node_modules,dashboard,extension,graphics,package.json,package-lock.json} build/nodecg/bundles/runback/
 cp -rf electron-builder build/nodecg/
 cp -rf electron-builder/electron-package.json build/nodecg/package.json
