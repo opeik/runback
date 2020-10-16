0873f07e6e776730ab4aca7eafa83f71d7eeea3b
Sandro Stikić <sandro@stikic.me>
2020-10-16 14:50:45 +0800

    Update build script

--- electron-builder/prepare-build.sh
+++ electron-builder/prepare-build.sh
@@ -5,8 +5,7 @@ cp -rf lib/nodecg build/
 cp -f electron-builder/electron-package.json build/nodecg/package.json
 
 if [ "$RUNNER_OS" == "Windows" ]; then
-    local runback_dir=$(pwd)
-    cp -rf "$runback_dir/../runback" build/nodecg/bundles/
+    cp -rf ../runback build/nodecg/bundles/
 
 else
     ln -sf ../../../../runback build/nodecg/bundles/
