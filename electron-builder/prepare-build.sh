847b295f6f7f1b31152074753ba09ee8c2cede12
Sandro Stikić <sandro@stikic.me>
2020-10-16 13:59:39 +0800

    Update build script

--- electron-builder/prepare-build.sh
+++ electron-builder/prepare-build.sh
@@ -3,6 +3,7 @@ set -euxo pipefail
 mkdir -p build
 cp -rf lib/nodecg build/
 cp -f electron-builder/electron-package.json build/nodecg/package.json
+rm -f build/nodecg/bundles/runback
 ln -sf ../../../../runback build/nodecg/bundles/
 cd build/nodecg && yarn install
 cd bundles/runback && yarn install
