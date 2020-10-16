4ec2d292ae90d9c19eaf29052d2699ade7528d0c
Sandro Stikić <sandro@stikic.me>
2020-10-16 14:39:33 +0800

    God help me, I will strangle windows

--- electron-builder/prepare-build.sh
+++ electron-builder/prepare-build.sh
@@ -3,9 +3,10 @@ set -euxo pipefail
 mkdir -p build
 cp -rf lib/nodecg build/
 cp -f electron-builder/electron-package.json build/nodecg/package.json
-rm -f build/nodecg/bundles/runback
 
 if [ "$RUNNER_OS" == "Windows" ]; then
+    ls ../../../
+    ls ../../../../
     cp -rf ../../../../runback build/nodecg/bundles/
 else
     ln -sf ../../../../runback build/nodecg/bundles/
